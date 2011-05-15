6d941c52ecfedc61cf63edf95749e45955e1e041
Tanguy <tanguy.pruvot@gmail.com>
2011-05-15 23:02:50 +0200

    sample merge cron

--- merge_cron.sh
+++ merge_cron.sh
@@ -3,11 +3,11 @@
 # git fetch cyanogen
 # git merge remotes/cyanogen/gingerbread
 
-git remote add tpruvot git://github.com/tpruvot/android.git
-git fetch tpruvot
-git merge remotes/tpruvot/gingerbread
+# git remote add tpruvot git://github.com/tpruvot/android.git
+# git fetch tpruvot
+# git merge remotes/tpruvot/gingerbread
 
-git remote add quarx git://github.com/Quarx2k/android.git
+# git remote add quarx git://github.com/Quarx2k/android.git
 git fetch quarx
 git merge remotes/quarx/gingerbread
 
